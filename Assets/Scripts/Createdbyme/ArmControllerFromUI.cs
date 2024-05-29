b1c92765a1fbe204b9c21510b8e8c45d35a24d78
DESKTOP-89N230N\SAT <Dani3dxd>
2024-05-29 13:15:39 -0500

    Change on script for cartesian movement, fix the origin position for the table, fix some dimensions on canvas for articular and cartesian, add a new canvas for all buttons in project, change on player settings for change the name of project in meta quest

--- Assets/Scripts/Createdbyme/ArmControllerFromUI.cs
+++ Assets/Scripts/Createdbyme/ArmControllerFromUI.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 public class ArmControllerFromUI : MonoBehaviour
 {
     //This part call each part of robot joints
-    [SerializeField] private Slider[] robotSlides = new Slider[6];
+    [SerializeField] private Slider[] articularSliders = new Slider[6];
 
     //This part call each part of values show in canvas to know what happen internaly with values changed on robot
     [SerializeField] private TMP_Text[] valueArticulations = new TMP_Text[6]; //Try to improve this part for call one time like the function before
@@ -20,7 +20,7 @@ public class ArmControllerFromUI : MonoBehaviour
     {
         DefineInitialValues();
         //define initial values
-        foreach (var slides in robotSlides)
+        foreach (var slides in articularSliders)
         {
             slides.onValueChanged.AddListener((float value) =>
             {
@@ -32,22 +32,22 @@ public class ArmControllerFromUI : MonoBehaviour
 
     void DefineInitialValues()
     {
-        for (int i = 0; i < robotSlides.Length; i++)
+        for (int i = 0; i < articularSliders.Length; i++)
         {
             if (i == 0 || i == 5)
             {
-                robotSlides[i].minValue = -180;
-                robotSlides[i].maxValue = 180;
+                articularSliders[i].minValue = 0;
+                articularSliders[i].maxValue = 360;
             }
             else if (i == 1)
             {
-                robotSlides[i].minValue = -180;
-                robotSlides[i].maxValue = 0;
+                articularSliders[i].minValue = -180;
+                articularSliders[i].maxValue = 0;
             }
             else
             {
-                robotSlides[i].minValue = -90;
-                robotSlides[i].maxValue = 90;
+                articularSliders[i].minValue = -90;
+                articularSliders[i].maxValue = 90;
             }
         }
     }
@@ -56,15 +56,12 @@ public class ArmControllerFromUI : MonoBehaviour
     {
         for (int i = 0; i < robotArticulations.Length; i++)
         {
-            valueArticulations[i].text = robotSlides[i].value.ToString("F2");
+            valueArticulations[i].text = articularSliders[i].value.ToString("F2");
             if (i == 0 || i == 4)
-                robotArticulations[i].transform.localRotation = Quaternion.Euler(0, 0, robotSlides[i].value);
+                robotArticulations[i].transform.localRotation = Quaternion.Euler(0, 0, articularSliders[i].value);
             else
-                robotArticulations[i].transform.localRotation = Quaternion.Euler(0, robotSlides[i].value, 0);
+                robotArticulations[i].transform.localRotation = Quaternion.Euler(0, articularSliders[i].value, 0);
         }
     }
-
-    /*
-    */
 
 }
--- Assets/Scripts/Createdbyme/UR3Controller.cs
+++ Assets/Scripts/Createdbyme/UR3Controller.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UR3Controller : MonoBehaviour
 {
-    public GameObject RobotBase; // Este se puede borrar
-    public GameObject controlCube;
-    public float[] jointValues = new float[6];
-    public float x = 0, y = 6, z = 3, phi = 0, theta = 0, psi;
-    public float oldX, oldY, oldZ, oldPhi, oldTheta, oldPsi;
-    //[Range(-10.0f, 10.0f)]
-    public string stringX, stringY, stringZ, stringPhi, stringTheta, stringPsi;
-    public bool userHasHitOk = false;
+    [SerializeField] private Slider[] cartesianSliders = new Slider[6];
+    [SerializeField] private TMP_Text[] valueCartesian = new TMP_Text[6];
+    [SerializeField] private GameObject[] robotCartesian = new GameObject[6];
+    [SerializeField] private Slider[] articularSliders = new Slider[6];
+    [SerializeField] private TMP_Text[] valueArticulations = new TMP_Text[6];
 
-    private int counter = 1;
-    private int frameCount = 200;
-    private float floatX, floatY, floatZ, floatPhi, floatTheta, floatPsi; //Procurar utilizar verdaderos flotantes ya que este programa solo reconoce enteros
+    public GameObject controlCube;
+    //public float[] jointValues = new float[6];
+    public float x = 0, y = 6, z = 3, phi = 0, theta = 0, psi;
+
     private GameObject[] jointList = new GameObject[6]; // Este es el que sirve para operar
     private UR3Solver Robot1 = new UR3Solver();
     private UR3Solver Robot11
@@ -34,146 +34,81 @@ public class UR3Controller : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        initializeJoints();
+        initializeValues();
         initializeCube();
-    }
-
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        controlCube.transform.position = new Vector3(floatX, floatY, floatZ);
-        controlCube.transform.eulerAngles = new Vector3(floatPhi, floatTheta, floatPsi);
-
-        oldX = x;
-        oldY = y;
-        oldZ = z;
-        oldPhi = phi;
-        oldTheta = theta;
-        oldPsi = psi;
-
-        if (userHasHitOk)
+        
+        foreach (var slides in cartesianSliders)
         {
-            if (counter >= frameCount)
+            slides.onValueChanged.AddListener((float value) => //Call it when change the value
             {
-                counter = 1;
-                userHasHitOk = false;
-            }
-
-            x = oldX + (counter * (controlCube.transform.position.x - oldX) / frameCount);
-            y = oldY + (counter * (controlCube.transform.position.y - oldY) / frameCount);
-            z = oldZ + (counter * (controlCube.transform.position.z - oldZ) / frameCount);
-            phi = controlCube.transform.eulerAngles.x * Mathf.Deg2Rad;
-            theta = controlCube.transform.eulerAngles.y * Mathf.Deg2Rad;
-            psi = controlCube.transform.eulerAngles.z * Mathf.Deg2Rad;
-            Debug.Log("psi = " + psi);
-
-            Robot11.Solve(x, y, z, phi, theta, psi);
-
-            for (int j = 0; j < 6; j++)
-            {
-                //modificar sobre este dependiendo del caso para cada eje
-                Vector3 currentRotation = jointList[j].transform.localEulerAngles;
-                if (j == 0 || j == 4)
-                {
-                    currentRotation.z = Robot11.solutionArray[j];
-                    jointList[j].transform.localEulerAngles = currentRotation;
-                }
-                else
-                {
-                    currentRotation.y = Robot11.solutionArray[j];
-                    jointList[j].transform.localEulerAngles = currentRotation;
-                }
-            }
-
-            counter++;
+                followCube();
+                IKMovement();
+            });
         }
     }
+    void followCube()
+    {
+        x = cartesianSliders[0].value;
+        y = cartesianSliders[1].value;
+        z = cartesianSliders[2].value;
+        phi = cartesianSliders[3].value;
+        theta = cartesianSliders[4].value;
+        psi = cartesianSliders[5].value;
 
+        controlCube.transform.position = new Vector3(x, y, z);
+        controlCube.transform.eulerAngles = new Vector3(phi, theta, psi);
+    }
+
+    void IKMovement()
+    {
+        
+        Robot11.Solve(x, y, z, phi * Mathf.Deg2Rad, theta * Mathf.Deg2Rad, psi * Mathf.Deg2Rad);
+
+        for (int j = 0; j < robotCartesian.Length; j++)
+        {
+            valueCartesian[j].text = cartesianSliders[j].value.ToString("F2");
+            if (j == 0 || j == 4)
+                robotCartesian[j].transform.localRotation = Quaternion.Euler(0, 0, Robot11.solutionArray[j]);
+            else
+                robotCartesian[j].transform.localRotation = Quaternion.Euler(0, Robot11.solutionArray[j], 0);
+            //articularSliders[j].value = Robot11.solutionArray[j];
+            //valueArticulations[j].text = Robot11.solutionArray[j].ToString("F2");
+        }
+
+    }
+    void initializeValues()
+    {
+        for (int i = 0; i < cartesianSliders.Length; i++)
+        {
+            if (i == 3||i ==5)
+            {
+                cartesianSliders[i].minValue = -90;
+                cartesianSliders[i].maxValue = 90;
+            }
+            else if(i == 4)
+            {
+                cartesianSliders[i].minValue = -180;
+                cartesianSliders[i].maxValue = 180;
+            }
+            else if(i == 1)
+            {
+                cartesianSliders[i].minValue = 0;
+                cartesianSliders[i].maxValue = 6;
+            }
+            else
+            {
+                cartesianSliders[i].minValue = -5;
+                cartesianSliders[i].maxValue = 5;
+            }
+        }
+    }
     void initializeCube()
     {
         controlCube = GameObject.Find("Target");
-        controlCube.transform.position = new Vector3(0f, 6f, 3f); //note, this is in format x, y, z - but y is up
+        controlCube.transform.position = new Vector3(2f, 1f, 0f); //note, this is in format x, y, z - but y is up
         controlCube.transform.localScale = new Vector3(.3f, 1f, .3f);
         controlCube.transform.eulerAngles = new Vector3(0f, 0f, 0f); //in degrees
-
-        floatX = 3f;
-        floatY = 4f;
-        floatZ = 3f;
-        floatPhi = 0f;
-        floatTheta = 0f;
-        floatPsi = 0f;
-    }
-
-    void OnGUI() // esta parte quitarla y cambiarla por los sliders
-    {
-        GUI.color = Color.black;
-
-        if (userHasHitOk)
-        {
-            //Esta parte permite convertir el valor ingresado en la interfaz en flotantes
-            floatX = float.Parse(stringX);
-            floatY = float.Parse(stringY);
-            floatZ = float.Parse(stringZ);
-            floatPhi = float.Parse(stringPhi);
-            floatTheta = float.Parse(stringTheta);
-            floatPsi = float.Parse(stringPsi);
-
-            //Debug.Log("floatZ = " + floatZ + " and floatY = " + floatY);
-        }
-        else
-        {
-            //En esta parte se visualiza la interfaz grafica
-            GUI.Label(new Rect(10, 10, 100, 30), "Enter X:");
-            stringX = GUI.TextField(new Rect(150, 10, 50, 25), stringX, 40);
-            GUI.Label(new Rect(10, 70, 150, 30), "Enter Psi (Degrees):");
-            stringPsi = GUI.TextField(new Rect(150, 70, 50, 25), stringPsi, 40);
-            GUI.Label(new Rect(10, 130, 100, 30), "Enter Y:");
-            stringY = GUI.TextField(new Rect(150, 130, 50, 25), stringY, 40);
-            GUI.Label(new Rect(10, 190, 150, 30), "Enter Theta (Degrees):");
-            stringTheta = GUI.TextField(new Rect(150, 190, 50, 25), stringTheta, 40);
-            GUI.Label(new Rect(10, 250, 100, 30), "Enter Z:");
-            stringZ = GUI.TextField(new Rect(150, 250, 50, 25), stringZ, 40);
-            GUI.Label(new Rect(10, 310, 150, 30), "Enter Phi (Degrees):");
-            stringPhi = GUI.TextField(new Rect(150, 310, 50, 25), stringPhi, 40);
-
-
-
-        }
-
-        if (GUI.Button(new Rect(10, 370, 50, 30), "Enter"))
-            userHasHitOk = true;
-    }
-
-    // Create the list of GameObjects that represent each joint of the robot
-    void initializeJoints()//Reemplazar esto para utilizar un array que permita cada conjunto de eje ponerlo aqui
-    {
-        var RobotChildren = RobotBase.GetComponentsInChildren<Transform>();
-        for (int i = 0; i < RobotChildren.Length; i++)
-        {
-            if (RobotChildren[i].name == "Joint1")
-            {
-                jointList[0] = RobotChildren[i].gameObject;
-            }
-            else if (RobotChildren[i].name == "Joint2")
-            {
-                jointList[1] = RobotChildren[i].gameObject;
-            }
-            else if (RobotChildren[i].name == "Joint3")
-            {
-                jointList[2] = RobotChildren[i].gameObject;
-            }
-            else if (RobotChildren[i].name == "Joint4")
-            {
-                jointList[3] = RobotChildren[i].gameObject;
-            }
-            else if (RobotChildren[i].name == "Joint5")
-            {
-                jointList[4] = RobotChildren[i].gameObject;
-            }
-            else if (RobotChildren[i].name == "Joint6")
-            {
-                jointList[5] = RobotChildren[i].gameObject;
-            }
-        }
+        cartesianSliders[0].value = controlCube.transform.position.x; cartesianSliders[1].value = controlCube.transform.position.y; cartesianSliders[2].value = controlCube.transform.position.z;
+        
     }
 }
