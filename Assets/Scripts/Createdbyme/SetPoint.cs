5f6a762232329cbba226a1c1b0d7e4bd2fba5e70
DESKTOP-89N230N\SAT <Dani3dxd>
2024-05-30 23:07:19 -0500

    Add new buttons for control the robot at scene like put the start position and end position to move between that 2 points, also i add a button to clear all the trajectories programated

---
+++ Assets/Scripts/Createdbyme/SetPoint.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SetPoint : MonoBehaviour
+{
+    [SerializeField] private LineRenderer lr; //load the component linerenderer at scene
+    [SerializeField] private GameObject sphere; //load the prefab for this case a sphere
+    [SerializeField] private Transform spherePosition;
+
+    private GameObject newSphere;
+    private List<GameObject> points = new List<GameObject>();
+    /// <summary>
+    /// This function calls when it pressed the button at scene
+    /// </summary>
+    public void instanceNewObject()
+    {
+        newSphere = Instantiate(sphere, spherePosition.transform.position, Quaternion.identity); //instance a new object taken the prefab that you load it before with a position and rotation stablished previously
+        points.Add(newSphere); // add a new object at list
+
+        lr.positionCount = points.Count; //stablished the quatity of points created in scene
+        for (int index = 0; index < lr.positionCount; index++)
+            lr.SetPosition(index, points[index].transform.position); // run the array and position points on stablished positions
+    }
+
+    public void CleanSpheres()
+    {
+        //points.Clear();
+        for (int i = 0; i < points.Count; i++)
+            GameObject.Destroy(points[i]);
+        for (int i = 0; i < points.Count; i++)
+            points.Clear();
+        lr.positionCount = 0;
+    }
+}
