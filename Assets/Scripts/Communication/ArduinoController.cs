ab39fc5e679c0d470ad45cf186374365325a2a96
Dani3dxd <daniel3d_07@hotmail.com>
2024-10-12 11:33:55 -0500

    Add arduino connection in a new scene to refresh, connect and disconnect serial ports with buttons, add new materials and prefabs to change the background and also change Y for Z axis to do correctly the robot movement

--- Assets/Scripts/Communication/ArduinoController.cs
+++ Assets/Scripts/Communication/ArduinoController.cs
@@ -1,44 +1,45 @@
 using System;
+using System.Collections.Generic;
 using System.IO.Ports;
 using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ArduinoController : MonoBehaviour
 {
-    [Header("Puerto Serial COM:")]
-    [SerializeField] private SerialPort serialPort;
-    [SerializeField] private String puertoCOM = "COM15";
-    [SerializeField] private int baudRate = 9600;
-    [Header("Nivel de Brillo: ")]
+    
+    SerialPort arduinoPort;
+    [Header("Puerto Serial COM")]
+    [SerializeField] private List<String> connectionPorts;
+    [SerializeField] private int baudRate = 115200;
+    [SerializeField] bool isConnected = false;
+
+    [Header("Buttons")]
+    [SerializeField] TMP_Dropdown dropList;
+    [SerializeField] Button connectBtn;
+    [SerializeField] Button disconnectBtn;
+
+    [Header("Nivel")]
     [SerializeField][Range(0, 255)] private int brightness1 = 0;
     [SerializeField][Range(0, 255)] private int brightness2 = 0;
+    
 
     void Start()
     {
-        // Inicializa el puerto serial
-        serialPort = new SerialPort(puertoCOM, baudRate);
-        serialPort.ReadTimeout = 500;
-        serialPort.WriteTimeout = 500;
-        try
-        {
-            serialPort.Open();
-            Debug.Log("Puerto serial abierto correctamente.");
-        }
-        catch (Exception e)
-        {
-            Debug.LogError("No se pudo abrir el puerto serial: " + e.Message);
-        }
+        RefreshPorts();
     }
 
     void Update()
     {
-        if (serialPort.IsOpen)
+
+        if (isConnected)
         {
             try
             {
                 // Enviar valores de brillo al Arduino
                 string brightnessData = brightness1.ToString() + "," + brightness2.ToString();
-                serialPort.WriteLine(brightnessData);
+                arduinoPort.WriteLine(brightnessData);
                 Debug.Log("Enviando brillo: " + brightnessData);
 
             }
@@ -47,13 +48,49 @@ public class ArduinoController : MonoBehaviour
                 Debug.LogError("Error al enviar datos al Arduino: " + e.Message);
             }
         }
+        connectBtn.gameObject.SetActive(!isConnected);
+        disconnectBtn.gameObject.SetActive(isConnected);
+    }
+
+    public void RefreshPorts()
+    {
+        connectionPorts = new List<String> { };
+        foreach (string port in SerialPort.GetPortNames())
+        {
+            connectionPorts.Add(port);
+        }
+        dropList.ClearOptions();
+        dropList.AddOptions(connectionPorts);
+    }
+
+    public void AttemptConnection()
+    {
+        arduinoPort = new SerialPort(dropList.captionText.text, baudRate);
+        try
+        {
+            arduinoPort.Open();
+            arduinoPort.WriteTimeout = 500;
+
+            Debug.Log("Puerto serial abierto correctamente.");
+            isConnected = true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("No se pudo abrir el puerto serial: " + e.Message);
+        }
+    }
+
+    public void Disconnect()
+    {
+        isConnected = false;
+        arduinoPort.Close();
     }
 
     void OnApplicationQuit()
     {
-        if (serialPort.IsOpen)
+        if (arduinoPort.IsOpen)
         {
-            serialPort.Close();
+            arduinoPort.Close();
             Debug.Log("Puerto serial cerrado.");
         }
     }
